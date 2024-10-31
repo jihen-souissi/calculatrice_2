e66a9eca9c914afee6d5ab7e40496806dbaf8ffb
jihen <jihen@JIHEN>
2024-10-31 19:36:27 +0100

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.vb
@@ -0,0 +1,274 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Text_affiche = New TextBox()
+        But_1 = New Button()
+        But_0 = New Button()
+        But_9 = New Button()
+        But_8 = New Button()
+        But_7 = New Button()
+        But_6 = New Button()
+        But_5 = New Button()
+        But_4 = New Button()
+        But_3 = New Button()
+        But_2 = New Button()
+        But_plus = New Button()
+        But_rap = New Button()
+        But_foi = New Button()
+        But_moin = New Button()
+        But_aq = New Button()
+        But_C = New Button()
+        SuspendLayout()
+        ' 
+        ' Text_affiche
+        ' 
+        Text_affiche.Location = New Point(154, 64)
+        Text_affiche.Name = "Text_affiche"
+        Text_affiche.Size = New Size(214, 27)
+        Text_affiche.TabIndex = 0
+        ' 
+        ' But_1
+        ' 
+        But_1.BackColor = Color.FromArgb(CByte(0), CByte(192), CByte(192))
+        But_1.ForeColor = Color.FromArgb(CByte(0), CByte(0), CByte(192))
+        But_1.Location = New Point(154, 139)
+        But_1.Name = "But_1"
+        But_1.Size = New Size(53, 44)
+        But_1.TabIndex = 1
+        But_1.Text = "1"
+        But_1.UseVisualStyleBackColor = False
+        ' 
+        ' But_0
+        ' 
+        But_0.BackColor = Color.FromArgb(CByte(0), CByte(192), CByte(192))
+        But_0.ForeColor = Color.FromArgb(CByte(0), CByte(0), CByte(192))
+        But_0.Location = New Point(233, 351)
+        But_0.Name = "But_0"
+        But_0.Size = New Size(53, 44)
+        But_0.TabIndex = 2
+        But_0.Text = "0"
+        But_0.UseVisualStyleBackColor = False
+        ' 
+        ' But_9
+        ' 
+        But_9.BackColor = Color.FromArgb(CByte(0), CByte(192), CByte(192))
+        But_9.ForeColor = Color.FromArgb(CByte(0), CByte(0), CByte(192))
+        But_9.Location = New Point(315, 282)
+        But_9.Name = "But_9"
+        But_9.Size = New Size(53, 44)
+        But_9.TabIndex = 3
+        But_9.Text = "9"
+        But_9.UseVisualStyleBackColor = False
+        ' 
+        ' But_8
+        ' 
+        But_8.BackColor = Color.FromArgb(CByte(0), CByte(192), CByte(192))
+        But_8.ForeColor = Color.FromArgb(CByte(0), CByte(0), CByte(192))
+        But_8.Location = New Point(233, 282)
+        But_8.Name = "But_8"
+        But_8.Size = New Size(53, 44)
+        But_8.TabIndex = 4
+        But_8.Text = "8"
+        But_8.UseVisualStyleBackColor = False
+        ' 
+        ' But_7
+        ' 
+        But_7.BackColor = Color.FromArgb(CByte(0), CByte(192), CByte(192))
+        But_7.ForeColor = Color.FromArgb(CByte(0), CByte(0), CByte(192))
+        But_7.Location = New Point(154, 282)
+        But_7.Name = "But_7"
+        But_7.Size = New Size(53, 44)
+        But_7.TabIndex = 5
+        But_7.Text = "7"
+        But_7.UseVisualStyleBackColor = False
+        ' 
+        ' But_6
+        ' 
+        But_6.BackColor = Color.FromArgb(CByte(0), CByte(192), CByte(192))
+        But_6.ForeColor = Color.FromArgb(CByte(0), CByte(0), CByte(192))
+        But_6.Location = New Point(315, 211)
+        But_6.Name = "But_6"
+        But_6.Size = New Size(53, 44)
+        But_6.TabIndex = 6
+        But_6.Text = "6"
+        But_6.UseVisualStyleBackColor = False
+        ' 
+        ' But_5
+        ' 
+        But_5.BackColor = Color.FromArgb(CByte(0), CByte(192), CByte(192))
+        But_5.ForeColor = Color.FromArgb(CByte(0), CByte(0), CByte(192))
+        But_5.Location = New Point(233, 211)
+        But_5.Name = "But_5"
+        But_5.Size = New Size(53, 44)
+        But_5.TabIndex = 7
+        But_5.Text = "5"
+        But_5.UseVisualStyleBackColor = False
+        ' 
+        ' But_4
+        ' 
+        But_4.BackColor = Color.FromArgb(CByte(0), CByte(192), CByte(192))
+        But_4.ForeColor = Color.FromArgb(CByte(0), CByte(0), CByte(192))
+        But_4.Location = New Point(154, 211)
+        But_4.Name = "But_4"
+        But_4.Size = New Size(53, 44)
+        But_4.TabIndex = 8
+        But_4.Text = "4"
+        But_4.UseVisualStyleBackColor = False
+        ' 
+        ' But_3
+        ' 
+        But_3.BackColor = Color.FromArgb(CByte(0), CByte(192), CByte(192))
+        But_3.ForeColor = Color.FromArgb(CByte(0), CByte(0), CByte(192))
+        But_3.Location = New Point(315, 139)
+        But_3.Name = "But_3"
+        But_3.Size = New Size(53, 44)
+        But_3.TabIndex = 9
+        But_3.Text = "3"
+        But_3.UseVisualStyleBackColor = False
+        ' 
+        ' But_2
+        ' 
+        But_2.BackColor = Color.FromArgb(CByte(0), CByte(192), CByte(192))
+        But_2.ForeColor = Color.Navy
+        But_2.Location = New Point(233, 139)
+        But_2.Name = "But_2"
+        But_2.Size = New Size(53, 44)
+        But_2.TabIndex = 11
+        But_2.Text = "2"
+        But_2.UseVisualStyleBackColor = False
+        ' 
+        ' But_plus
+        ' 
+        But_plus.BackColor = Color.Cyan
+        But_plus.ForeColor = Color.Blue
+        But_plus.Location = New Point(497, 82)
+        But_plus.Name = "But_plus"
+        But_plus.Size = New Size(94, 29)
+        But_plus.TabIndex = 13
+        But_plus.Text = "+"
+        But_plus.UseVisualStyleBackColor = False
+        ' 
+        ' But_rap
+        ' 
+        But_rap.BackColor = Color.Cyan
+        But_rap.ForeColor = Color.Blue
+        But_rap.Location = New Point(619, 139)
+        But_rap.Name = "But_rap"
+        But_rap.Size = New Size(94, 29)
+        But_rap.TabIndex = 14
+        But_rap.Text = "/"
+        But_rap.UseVisualStyleBackColor = False
+        ' 
+        ' But_foi
+        ' 
+        But_foi.BackColor = Color.Cyan
+        But_foi.ForeColor = Color.Blue
+        But_foi.Location = New Point(619, 82)
+        But_foi.Name = "But_foi"
+        But_foi.Size = New Size(94, 29)
+        But_foi.TabIndex = 15
+        But_foi.Text = "*"
+        But_foi.UseVisualStyleBackColor = False
+        ' 
+        ' But_moin
+        ' 
+        But_moin.BackColor = Color.Cyan
+        But_moin.ForeColor = Color.Blue
+        But_moin.Location = New Point(497, 139)
+        But_moin.Name = "But_moin"
+        But_moin.Size = New Size(94, 29)
+        But_moin.TabIndex = 16
+        But_moin.Text = "-"
+        But_moin.UseVisualStyleBackColor = False
+        ' 
+        ' But_aq
+        ' 
+        But_aq.BackColor = Color.Cyan
+        But_aq.ForeColor = Color.Blue
+        But_aq.Location = New Point(497, 195)
+        But_aq.Name = "But_aq"
+        But_aq.Size = New Size(94, 29)
+        But_aq.TabIndex = 17
+        But_aq.Text = "="
+        But_aq.UseVisualStyleBackColor = False
+        ' 
+        ' But_C
+        ' 
+        But_C.BackColor = Color.Cyan
+        But_C.ForeColor = Color.Blue
+        But_C.Location = New Point(619, 195)
+        But_C.Name = "But_C"
+        But_C.Size = New Size(94, 29)
+        But_C.TabIndex = 18
+        But_C.Text = "C"
+        But_C.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(192), CByte(255), CByte(255))
+        ClientSize = New Size(800, 450)
+        Controls.Add(But_C)
+        Controls.Add(But_aq)
+        Controls.Add(But_moin)
+        Controls.Add(But_foi)
+        Controls.Add(But_rap)
+        Controls.Add(But_plus)
+        Controls.Add(But_2)
+        Controls.Add(But_3)
+        Controls.Add(But_4)
+        Controls.Add(But_5)
+        Controls.Add(But_6)
+        Controls.Add(But_7)
+        Controls.Add(But_8)
+        Controls.Add(But_9)
+        Controls.Add(But_0)
+        Controls.Add(But_1)
+        Controls.Add(Text_affiche)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Text_affiche As TextBox
+    Friend WithEvents But_1 As Button
+    Friend WithEvents But_0 As Button
+    Friend WithEvents But_9 As Button
+    Friend WithEvents But_8 As Button
+    Friend WithEvents But_7 As Button
+    Friend WithEvents But_6 As Button
+    Friend WithEvents But_5 As Button
+    Friend WithEvents But_4 As Button
+    Friend WithEvents But_3 As Button
+    Friend WithEvents But_2 As Button
+    Friend WithEvents But_plus As Button
+    Friend WithEvents But_rap As Button
+    Friend WithEvents But_foi As Button
+    Friend WithEvents But_moin As Button
+    Friend WithEvents But_aq As Button
+    Friend WithEvents But_C As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,155 @@
+﻿Imports System.Reflection.Emit
+Public Class Form1
+    Dim A As String
+    Dim B As String
+    Dim op = ""
+    Private Sub But_0_Click(sender As Object, e As EventArgs) Handles But_0.Click
+        If op = "" Then
+            A = A + "0"
+        Else
+            B = B + "0"
+        End If
+        Text_affiche.Text = A + " " + op + " " + B
+    End Sub
+    Private Sub But_1_Click(sender As Object, e As EventArgs) Handles But_1.Click
+        If op = "" Then
+            A = A + "1"
+        Else
+            B = B + "1"
+        End If
+        Text_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub But_2_Click(sender As Object, e As EventArgs) Handles But_2.Click
+        If op = "" Then
+            A = A + "2"
+        Else
+            B = B + "2"
+        End If
+        Text_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub But_3_Click(sender As Object, e As EventArgs) Handles But_3.Click
+        If op = "" Then
+            A = A + "3"
+        Else
+            B = B + "3"
+        End If
+        Text_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub But_4_Click(sender As Object, e As EventArgs) Handles But_4.Click
+        If op = "" Then
+            A = A + "4"
+        Else
+            B = B + "4"
+        End If
+        Text_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub But_5_Click(sender As Object, e As EventArgs) Handles But_5.Click
+        If op = "" Then
+            A = A + "5"
+        Else
+            B = B + "5"
+        End If
+        Text_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub But_6_Click(sender As Object, e As EventArgs) Handles But_6.Click
+        If op = "" Then
+            A = A + "6"
+        Else
+            B = B + "6"
+        End If
+        Text_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub But_7_Click(sender As Object, e As EventArgs) Handles But_7.Click
+        If op = "" Then
+            A = A + "7"
+        Else
+            B = B + "7"
+        End If
+        Text_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub But_8_Click(sender As Object, e As EventArgs) Handles But_8.Click
+        If op = "" Then
+            A = A + "8"
+        Else
+            B = B + "8"
+        End If
+        Text_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub But_9_Click(sender As Object, e As EventArgs) Handles But_9.Click
+        If op = "" Then
+            A = A + "9"
+        Else
+            B = B + "9"
+        End If
+        Text_affiche.Text = A + " " + op + " " + B
+    End Sub
+    Private Sub But_plus_Click(sender As Object, e As EventArgs) Handles But_plus.Click
+        If Not A = "" Then
+            op = "+"
+        End If
+        Text_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub But_moin_Click(sender As Object, e As EventArgs) Handles But_moin.Click
+        If Not A = "" Then
+            op = "-"
+        End If
+        Text_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub But_foi_Click(sender As Object, e As EventArgs) Handles But_foi.Click
+        If Not A = "" Then
+            op = "*"
+        End If
+        Text_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub But_rap_Click(sender As Object, e As EventArgs) Handles But_rap.Click
+        If Not A = "" Then
+            op = "/"
+        End If
+        Text_affiche.Text = A + " " + op + " " + B
+    End Sub
+    Private Sub But_C_Click(sender As Object, e As EventArgs) Handles But_C.Click
+        Text_affiche.Text = ""
+        A = ""
+        B = ""
+        op = ""
+    End Sub
+
+    Private Sub But_aq_Click(sender As Object, e As EventArgs) Handles But_aq.Click
+        Dim test = True
+        Dim res As Double
+        If IsNumeric(A) And IsNumeric(B) And Not op = "" Then
+            Dim va = Integer.Parse(A)
+            Dim vb = Integer.Parse(B)
+            If op = "+" Then
+                res = va + vb
+            ElseIf op = "-" Then
+                res = va - vb
+            ElseIf op = "*" Then
+                res = va * vb
+            ElseIf op = "/" And Not op = "" Then
+                res = va / vb
+            Else
+                MessageBox.Show("Division par 0")
+                test = False
+            End If
+        Else
+            MessageBox.Show("A et B doivent etre des entiers")
+            test = False
+        End If
+        If test Then
+            Text_affiche.Text = Text_affiche.Text + "=" + res.ToString
+        End If
+
+    End Sub
+End Class
